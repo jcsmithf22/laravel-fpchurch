3471c5473128dacc3de7d6f67f3f84fb22e517b1
Josiah Smith <jsmith@Josiahs-Air.lan>
2024-07-09 20:45:46 -0500

    add local fonts

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,10 +10,11 @@
     <!-- Fonts -->
     {{--    <link rel="preconnect" href="https://fonts.bunny.net">--}}
     {{--    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>--}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap"
-          rel="stylesheet">
+{{--    <link rel="preconnect" href="https://fonts.googleapis.com">--}}
+{{--    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>--}}
+{{--    <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap"--}}
+{{--          rel="stylesheet">--}}
+{{--    <link rel="preload" href="{{ asset("fonts/Figtree-VariableFont_wght.ttf") }}" as="font" type="font/woff2" crossorigin>--}}
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
